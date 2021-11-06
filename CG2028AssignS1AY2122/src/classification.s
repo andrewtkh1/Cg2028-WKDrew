f0424e1582487d1cdf8f7c48478973893494ef2b
andrewtkh1 <andrewtkh1@gmail.com>
2021-11-06 08:24:12 +0800

    Added Machine language to maincode

--- CG2028AssignS1AY2122/src/classification.s
+++ CG2028AssignS1AY2122/src/classification.s
@@ -37,8 +37,8 @@ classification:
 			BL LOADSAMPLE 			@Load in X1, Y1
 			PUSH {R0,R4} 			@Save pointer of dist array & N to 8
 CALCDIST:	BL SETDISTANCE 			@Loop to set distance array
-			SUBS R0, #1
-			BNE CALCDIST 			@Once done: R5,R6 is free.
+			SUBS R0, #1																		@0b0000 0010 0101 0000 0000 0000 0000 0001
+			BNE CALCDIST 			@Once done: R5,R6 is free.								@0b0001 1000 0000 0000 0000 0000 0000 1100
 			POP {R0,R4} 			@Set pointer back to start of dist array & N to 8
 			LDR R1, =SHORTESTKDIST	@Load array of size 100
 			PUSH {R0-R7}
@@ -51,10 +51,10 @@ CALCDIST:	BL SETDISTANCE 			@Loop to set distance array
 									@R4 - Array of Dist
 									@R5:R9 - Free
 			PUSH {R0-R4}
-			MOV R8, #0				@Set R8 to 0 first.
+			MOV R8, #0				@Set R8 to 0 first.										@0b0000 0011 1010 0000 1000 0000 0000 0000
 			BL SETLABELS			@Gets the Shortest Label
 			POP {R0-R4}
-			MOV R0, R8
+			MOV R0, R8																		@0b0000 0001 1010 0000 0000 0000 0000 1000
 @ POP / restore original register values. DO NOT save or restore R0. Why?
 			POP {R1-R9,LR}
 @ return to C program
@@ -70,21 +70,21 @@ CALCDIST:	BL SETDISTANCE 			@Loop to set distance array
 
 SETLABELS:
 			PUSH {R0,R2,R4}
-			LDR R5, [R1]	 		@Load 1st val
+			LDR R5, [R1]	 		@Load 1st val											@0b0000 0101 1001 0001 0101 0000 0000 0000
 LOOPNARR:							@Loop N times
-			LDR	R7, [R4], #4 		@Get CurDist
-			LDR R6, [R2], #4 		@Get Label
-			CMP R7, R5
+			LDR	R7, [R4], #4 		@Get CurDist											@0b0000 0100 1001 0100 0111 0000 0000 0100
+			LDR R6, [R2], #4 		@Get Label												@0b0000 0100 1001 0010 0110 0000 0000 0100
+			CMP R7, R5																		@0b0000 0001 0101 0111 0000 0000 0000 0101
 			ITTTT EQ
-			MOVEQ R9, R5			@Set R9 to shortest.
-			MOVEQ R8, R6			@Set Label
+			MOVEQ R9, R5			@Set R9 to shortest.									@0b0000 0001 1010 0000 1001 0000 0000 0101
+			MOVEQ R8, R6			@Set Label												@0b0000 0001 1010 0000 1000 0000 0000 0110
 			POPEQ {R0,R2,R4}		@POP back Reg.
 			BXEQ LR					@Return back
-			SUBS R0, #1
-			BNE LOOPNARR			@End loop N times
+			SUBS R0, #1																		@0b0000 0010 0101 0000 0000 0000 0000 0001
+			BNE LOOPNARR			@End loop N times										@0b0001 1000 0000 0000 0000 0000 0010 1100
 			POP {R0,R2,R4}			@Pop if Never found
-			MOV R9, #0
-			MOV R8, #2
+			MOV R9, #0																		@0b0000 0011 1010 0000 1001 0000 0000 0000
+			MOV R8, #2																		@0b0000 0011 1010 0000 1000 0000 0000 0010
 			BX LR					@Return label -2 if not found
 
 @R0 - N(Int)
@@ -92,19 +92,19 @@ LOOPNARR:							@Loop N times
 @R4 - Dist(Array)
 @R5/R6/R7 - Working (Unused ,curDist in K , curDist in N)
 GETDIST:							@Load first dist into K-array
-		LDR R7, [R4], #4
-		STR R7, [R1]
-		SUB R0, #1					@Decrease N
+		LDR R7, [R4], #4																	@0b0000 0100 1001 0100 0111 0000 0000 0100
+		STR R7, [R1]																		@0b0000 0101 1000 0001 0111 0000 0000 0000
+		SUB R0, #1					@Decrease N												@0b0000 0010 0100 0000 0000 0000 0000 0001
 REMAIN:
-		LDR R7, [R4], #4			@Load next from DistN
-		LDR R6, [R1]				@Load Cur item from ShortestK
-		CMP R7, R6
+		LDR R7, [R4], #4			@Load next from DistN									@0b0000 0100 1001 0100 0111 0000 0000 0100
+		LDR R6, [R1]				@Load Cur item from ShortestK							@0b0000 0101 1001 0001 0110 0000 0000 0000
+		CMP R7, R6																			@0b0000 0001 0101 0111 0000 0000 0000 0110
 		IT LT						@If R7 LessThan R6
-		STRLT R7,[R1] 				@Store R7
-		SUBS R0, #1					@Decrease N
+		STRLT R7,[R1] 				@Store R7												@0b0000 0101 1000 0001 0111 0000 0000 0000
+		SUBS R0, #1					@Decrease N												@0b0000 0010 0101 0000 0000 0000 0000 0001
 		IT EQ
 		BXEQ LR						@Return If N = 0
-		B REMAIN					@End of Whole N loop
+		B REMAIN					@End of Whole N loop									@0b1110 1000 0000 0000 0000 0000 0010 1000
 
 @R0 - N(Int)
 @R1 - Points(Array)
@@ -114,22 +114,22 @@ REMAIN:
 @R7 - X1, X2
 SETDISTANCE:
 		PUSH {R5-R6}
-		LDR R7, [R1], #4 			@Get x2
-		SUB R5, R7
-		MUL R5, R5 					@Store (x1-x2)^2 into R5
-		LDR R7, [R1], #4 			@get y2
-		SUB R6, R7 	   				@Value of y1 - y2
-		MLA R5, R6, R6, R5 			@R6^2 + R5 into R5.
-		STR R5, [R4], #4 			@put value into Memory
+		LDR R7, [R1], #4 			@Get x2													@0b0000 0100 1001 0001 0111 0000 0000 0100
+		SUB R5, R7																			@0b0000 0000 0100 0101 0101 0000 0000 0111
+		MUL R5, R5 					@Store (x1-x2)^2 into R5								@0b0000 0000 0000 0000 0101 0101 0001 0101
+		LDR R7, [R1], #4 			@get y2													@0b0000 0100 1001 0001 0111 0000 0000 0100
+		SUB R6, R7 	   				@Value of y1 - y2										@0b0000 0000 0100 0110 0110 0000 0000 0111
+		MLA R5, R6, R6, R5 			@R6^2 + R5 into R5.										@0b0000 0000 0010 0101 0101 0110 0001 0110
+		STR R5, [R4], #4 			@put value into Memory									@0b0000 0100 1000 0100 0101 0000 0000 0100
 		POP {R5-R6}
 		BX LR
 
 LOADSAMPLE:
 		@Load Sample X,Y,K
-		LDR R5, [R3], #4
-		LDR R6, [R3], #4
-		LDR R7, [R3], #4
-		MOV R3, R7
+		LDR R5, [R3], #4																	@0b0000 0100 1001 0011 0101 0000 0000 0100
+		LDR R6, [R3], #4																	@0b0000 0100 1001 0011 0110 0000 0000 0100
+		LDR R7, [R3], #4																	@0b0000 0100 1001 0011 0111 0000 0000 0100
+		MOV R3, R7																			@0b0000 0001 1010 0000 0011 0000 0000 0111
 		BX LR
 
 
